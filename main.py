ed55186c2d7984798ccad474acb67a9e38021fc0
glemiu6 <digorivlad59@gmail.com>
2026-04-28 04:22:31 +0100

    logger and typos correction

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 #! /usr/bin/env python3
+import logging
+from pyragcore.utils_io.logger import get_logger,set_up_logging
+set_up_logging(level=logging.INFO)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("huggingface_hub").setLevel(logging.WARNING)
+logging.getLogger("sentence_transformers").setLevel(logging.WARNING)
+logger = get_logger(__name__)
 from rag_pipeline import RagPipeline
 import sys
-from ragcore.exceptions import BotRagException
+from pyragcore.exceptions import BotRagException
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python3 main.py <persist_dir> <output_folder>")
         sys.exit(1)
-
+    logger.info("Starting bot-rag")
     rag = RagPipeline(sys.argv[1],sys.argv[2])
     chat_history=[]
     try:
@@ -24,10 +33,10 @@ if __name__ == "__main__":
 
     if input("Choose mode:\n1. Write \n2. Speak\n >").strip() == '1':
         while True:
-            query=input("Enter question: ")
+            query=input("Enter question or exit (exit,quit or q): ")
             if query.lower() in ('exit','quit','q'):
                 break
-            ans=rag.ask(question=query,source_id=source_id,chat_history=chat_history)
+            ans=rag.ask(question=query,source_id=source_id,chat_history=chat_history,stream=True)
             print(ans)
             chat_history.append({"role":"user","message":query})
             chat_history.append({"role":"assistant","message":ans})
@@ -43,5 +52,5 @@ if __name__ == "__main__":
             rag.say(ans)
             chat_history.append({"role":"user","message":query})
             chat_history.append({"role":"assistant","message":ans})
-    if input("Do you want to save the output? (y/n)").strip().lower() == 'y':
+    if input("Do you want to save the output? (y/n)\n\n>").strip().lower() == 'y':
         rag.save(chat_history)
